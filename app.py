f6a2cf5303da185ae05ecf0dbadd9e39de5ce18a
annatyrchan <antyrchan@gmail.com>
2023-05-20 12:44:37 +0000

    age verification

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:")
-# Sprawdzamy czy podany wiek jest liczbą
+wiek = input("Podaj wiek:")
 if wiek.isdigit() == False:
-	exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
+	exit("Wiek musi być liczba calkowita")
 wiek=int(wiek)
-if wiek>=18 and wiek<40:
-	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-elif wiek>=40:
-	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-	print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
+if wiek>=18:
+	print("mozesz kupic aperol w Europie")
 else:
-  exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
+	exit("nie mozesz kupic alkoholu w Europie")
