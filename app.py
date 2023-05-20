4d4383b70d57c1bb618b94cd911373c138ae45a0
KingaNguyenCong <kingagalicaa@gmail.com>
2023-05-20 12:29:40 +0000

    Gender selection

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:")
-# Sprawdzamy czy podany wiek jest liczbą
-if wiek.isdigit() == False:
-	exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
-wiek=int(wiek)
-if wiek>=18 and wiek<40:
-	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-elif wiek>=40:
-	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-	print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
+# Prośba o wprowadzenie płci
+plec = input("Wybierz płeć (mężczyzna/kobieta): ")
+
+# Sprawdzenie wyboru płci
+if plec == "mężczyzna":
+    print("Wybrałeś mężczyznę.")
+   
+elif plec == "kobieta":
+    print("Wybrałaś kobietę.")
+ 
 else:
-  exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
+    print("Niepoprawny wybór płci.")
+  
