9e45f0a6d952e8fa46671fe813a60486a3ce501d
maciejsedziak <maciej.sedziak@gmail.com>
2023-05-20 12:46:09 +0000

    age 120

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:")
 if wiek.isdigit() == False:
 	exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
 wiek=int(wiek)
-if wiek>=18 and wiek<40:
+if wiek>=18 and wiek<40 and wiek<=119:
 	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-elif wiek>=40:
+elif wiek>=40 and wiek<=119:
 	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
 	print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
+elif wiek>=120:
+	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+	print("Gratulujemy pieknego wieku!")
 else:
   exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
+
